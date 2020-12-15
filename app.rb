edcca77c29d05344685ef9435744a10f074cff4e
Wesley <wspencer3295@yahoo.com>
2020-12-15 14:59:12 -0700

    commit tree 72086572e8a30de9f38bc8284ddd12f824b4356c

--- app.rb
+++ app.rb
@@ -2,5 +2,54 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+get "/reversename/:name" do
+  @name = params[:name]
+  "#{@name.reverse}"
+
+end
+
+get "/square/:number" do 
+  @number = params[:number].to_i
+"#{@number*@number}"
+end
+
+get "/say/:number/:phrase" do 
+  str = " "
+  @number = params[:number].to_i
+  @phrase = params[:phrase].to_s
+  @number.times do 
+    str += "#{@phrase}"
+  end
+  str
+end
+
+get "/say/:word1/:word2/:word3/:word4/:word5" do
+  @word1 = params[:word1]
+  @word2 = params[:word2]
+  @word3 = params[:word3]
+  @word4 = params[:word4]
+  @word5 = params[:word5]
+  "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+end
+
+get "/:operation/:number1/:number2" do
+  @operation = params[:operation]
+  @number1 = params[:number1].to_i
+  @number2 = params[:number2].to_i
+  if @operation == "add"
+   equals = (@number1 + @number2).to_s
+   equals
+  elsif @operation == "multiply"
+    equals = (@number1 * @number2).to_s
+    equals
+  elsif @operation == "subtract"
+    equals = (@number2 - @number1).to_s
+    equals
+  elsif @operation == "divide"
+    equals = (@number1 / @number2).to_s
+    equals
+  end
+  end
+
 
 end
